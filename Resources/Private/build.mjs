a4a1e3318ff8b89fa9b7dc6d399e5d0a6102e82c
Jon Uhlmann <account@uhlmann.pro>
2025-07-04 21:51:44 +0200

    TASK: Optimize build script

--- Resources/Private/build.mjs
+++ Resources/Private/build.mjs
@@ -1,89 +1,47 @@
 import esbuild from 'esbuild'
 import { sassPlugin } from 'esbuild-sass-plugin'
-import extensibilityMap from '@neos-project/neos-ui-extensibility/extensibilityMap.json' assert { type: 'json' }
+import extensibilityMap from '@neos-project/neos-ui-extensibility/extensibilityMap.json' with { type: 'json' }
 
 const isWatchMode = process.argv.includes('--watch')
 
 /**
- * This file contains the JS/CSS bundler configuration, as executed on `npm run build`
+ * This file contains the JS/CSS bundler configuration, as executed on `yarn build`
  */
-/* esbuild
-    .build({
-        // Generic Options (shared between build.js and build-watch.js)
-        entryPoints: {
-            BackendModal: './BackendModal/src/index.js',
-            main: './main.scss'
-        },
-        sourceRoot: './BackendModal/src',
-        target: ['esnext'],
-        // To prevent shortening of top, right, bottom, left into inset because it is not well supported yet (https://github.com/evanw/esbuild/pull/1758/files)
-        supported: { 'inset-property': false },
-        bundle: true,
-        sourcemap: isWatchMode,
-        plugins: [sassPlugin({ watch: isWatchMode })],
-        outdir: '../Public/',
-        loader: {
-            '.js': 'jsx',
-            '.json': 'json'
-        },
 
-        // Specific options for "npm run build"
-        minify: false,
-        //watch: isWatchMode,
-        external: ['react'],
-        alias: extensibilityMap
-    })
-    .then(ctx => ctx.watch())
-    .catch(() => process.exit(1)) */
+const defaultOptions = {
+    entryPoints: {
+        BackendModal: './BackendModal/src/index.js',
+        main: './main.ts',
+        styles: './main.scss'
+    },
+    sourceRoot: './BackendModal/src',
+    target: ['esnext'],
+    supported: { 'inset-property': false },
+    bundle: true,
+    sourcemap: true,
+    plugins: [sassPlugin({ watch: isWatchMode })],
+    outdir: '../Public/',
+    loader: {
+        '.js': 'jsx',
+        '.json': 'json'
+    },
+    minify: false,
+    external: ['react', '/_Resources/*'],
+    alias: extensibilityMap
+}
+
 
 if (isWatchMode) {
     console.log('Watching for changes...');
     esbuild
-        .context({
-            entryPoints: {
-                BackendModal: './BackendModal/src/index.js',
-                main: './main.ts',
-                styles: './main.scss'
-            },
-            sourceRoot: './BackendModal/src',
-            target: ['esnext'],
-            supported: { 'inset-property': false },
-            bundle: true,
-            sourcemap: true,
-            plugins: [sassPlugin({ watch: true })],
-            outdir: '../Public/',
-            loader: {
-                '.js': 'jsx',
-                '.json': 'json'
-            },
-            minify: false,
-            external: ['react', '/_Resources/*'],
-            alias: extensibilityMap
-        })
+        .context(defaultOptions)
         .then(ctx => ctx.watch())
         .catch(() => process.exit(1));
 } else {
     esbuild
         .build({
-            entryPoints: {
-                BackendModal: './BackendModal/src/index.js',
-                main: './main.ts',
-                styles: './main.scss'
-            },
-            sourceRoot: './BackendModal/src',
-            target: ['esnext'],
-            supported: { 'inset-property': false },
-            bundle: true,
-            sourcemap: false,
-            plugins: [sassPlugin()],
-            outdir: '../Public/',
-            loader: {
-                '.js': 'jsx',
-                '.json': 'json'
-            },
-            minify: false,
-            external: ['react', '/_Resources/*'],
-            alias: extensibilityMap
+            ...defaultOptions,
+            minify: true,
         })
         .catch(() => process.exit(1));
 }
